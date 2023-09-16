dfb29365526006ae13cf0bae4ad4c9b0a52f1cc9
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2023-09-15 23:35:53 -0300

    entregablev1.0

---
+++ entregable.js
@@ -0,0 +1,46 @@
+class ProductManager {
+    constructor() {
+        this.products = [];
+    }
+    
+    getProducts() {
+        console.log('Products : ', this.products);
+    }
+    
+    addProduct(title, description, price, thumbnail, code, stock) {
+        if(this.products.find(product => product.code ===code)) {
+            console.log(`Error: The product code ${code} already exists`)
+        }else if(title==="" || description==="" || price==="" || thumbnail==="" || code==="" || stock==="" ){
+            console.log('Error: One or more field is empty, the product is not added')    
+        }else{
+            this.products.push({
+                id: this.products.length + 1,
+                title,
+                description,
+                price,
+                thumbnail,
+                code,
+                stock,
+            });
+            console.log(`Product code ${code} its added`)
+        }
+    }
+    getProductById(id) {
+        this.products.find(product=> {product.id === id ? console.log(product) : console.log(`Error: The product id ${id} is not found`)})
+    }
+}
+
+products = new ProductManager();
+products.getProducts()
+
+products.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25)
+products.getProducts()
+
+products.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25)
+products.getProducts()
+
+products.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen',"",  25)
+products.getProducts()
+
+products.getProductById(2)
+products.getProductById(1)
