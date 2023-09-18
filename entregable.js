b97bf3001cbec237de99c829ea2d8c2d1067059f
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2023-09-18 15:13:09 -0300

    modificacionValidacion

--- entregable.js
+++ entregable.js
@@ -10,7 +10,7 @@ class ProductManager {
     addProduct(title, description, price, thumbnail, code, stock) {
         if(this.products.find(product => product.code ===code)) {
             console.log(`Error: The product code ${code} already exists`)
-        }else if(title===undefined || description===undefined || price===undefined || thumbnail===undefined || code===undefined || stock===undefined ){
+        }else if(!title || !description || !price || !thumbnail || !code || !stock ){
             console.log('Error: One or more field is empty, the product is not added')    
         }else{
             this.products.push({
@@ -28,6 +28,7 @@ class ProductManager {
     getProductById(id) {
         this.products.find(product=> {product.id === id ? console.log(product) : console.log(`Error: The product id ${id} is not found`)})
     }
+    
 }
 
 products = new ProductManager();
@@ -40,7 +41,9 @@ products.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin i
 products.getProducts()
 
 products.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen')
+products.addProduct('', null, 200, '')
 products.getProducts()
 
 products.getProductById(2)
 products.getProductById(1)
+
