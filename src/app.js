45b9095fa2e6f8e97e3394cb819a6e94422f2a08
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2023-10-03 22:23:18 -0300

    updatesv1.0.3

--- src/app.js
+++ src/app.js
@@ -1,84 +1,60 @@
 //Entregable 3 Express BackEnd Coder
 //Server Express
-
-const express = require('express');
+import express from "express";
 const app = express();
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 
 //Importo la clase productManager
-
-const productManager = require('./ProductManager');
+import productManager from "./ProductManager.js";
 const testingProducts = new productManager("./data.json");
-// const products = require('./data.json')
-let productos;
-
-/* const testingJSON = async () => {
-    try {
-        const testingProducts = new productManager("./data.json");
-        const products = await testingProducts.getProducts();
-        console.log("getProducts", 'The products are: ', products);
-        productos = products;
-        return productos;
-    } catch (error) {
-        console.error(' Error: ', error.message);
-    }
-    
-};
-
-testingJSON() */
 
 //EndPoints
 
-app.get('/', (req, res) => {
-    const htmlText = '<h1 style="color:blue">Entrega 3 servidor express</h1>'
-    res.send(htmlText)
+app.get("/", (req, res) => {
+  const htmlText = '<h1 style="color:blue">Entrega 3 servidor express</h1>';
+  res.send(htmlText);
 });
 
-
-app.get('/products/:pid', async (req, res) => {
-    try {
-        let {pid} = req.params
-        // const testingProducts = new productManager("./data.json");
-        pid = parseInt(pid)
-        const productID = await testingProducts.getProductById(pid)
-
-        if (!productID){
-            res.json({
-                error: 'Product Not Found',
-                message: `The product id ${pid} not found`
-            })
-        }else{
-            res.send(productID);
-        }
-    }catch(error) {
-        res.status(400).json({
-            status: 'error',
-            message: error.message,
-        })
-    }   
+app.get("/products/:pid", async (req, res) => {
+  try {
+    let { pid } = req.params;
+    pid = parseInt(pid);
+    const productID = await testingProducts.getProductById(pid);
+    if (!productID) {
+      res.json({
+        error: "Product Not Found",
+        message: `The product id ${pid} not found`,
+      });
+    } else {
+      res.send(productID);
+    }
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: error.message,
+    });
+  }
 });
 
-app.get('/products',  async (req, res) => {
-    try {
-        // const testingProducts = new productManager("./data.json");
-        const products = await testingProducts.getProducts();
-        const {limit} = req.query;
-        if (limit <= 10) {
-            prodFilter = products.filter(p => p.id <= limit)
-            res.send(prodFilter)
-        }else{
-            res.send(products)
-        }        
-    }catch(error) {
-        res.status(400).json({
-            status: 'error',
-            message: error.message,
-        })
-    }   
+app.get("/products", async (req, res) => {
+  try {
+    const products = await testingProducts.getProducts();
+    const { limit } = req.query;
+    let prodFilter;
+    if (limit <= 10) {
+      prodFilter = products.filter((p) => p.id <= limit);
+      res.send(prodFilter);
+    } else {
+      res.send(products);
+    }
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: error.message,
+    });
+  }
 });
 
 app.listen(8080, () => {
-    console.log('Server listening at port 8080')
+  console.log("Server listening at port 8080");
 });
-
-
