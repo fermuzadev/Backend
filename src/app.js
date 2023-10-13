aecd6751274487fcc01f67dee26b1e9218d4df23
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2023-10-12 22:28:03 -0300

    updatesWebsocket

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import handlebars from "express-handlebars";
 import path from "path";
 
-import chatRouter from "../routes/chat.routers.js";
+import chatRouter from "./routers/chat.routers.js";
 import { __dirname } from "./utils.js";
 
 const app = express();
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.engine("handlebars", handlebars.engine());
-app.set("views", path.join(__dirname, "./views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "handlebars");
 
 app.use("/", chatRouter);
