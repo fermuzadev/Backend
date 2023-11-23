b66355f0abe83e5b28bf15dfbbd797f850c8d31e
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2023-11-23 08:40:05 -0300

    updates

---
+++ src/app.js
@@ -0,0 +1,79 @@
+import express from "express";
+import expressSession from "express-session";
+import handlebars from "express-handlebars";
+import path from "path";
+import MongoStore from "connect-mongo";
+import { __dirname } from "./utils.js";
+import { uploader } from "./utils.js";
+import prodRouter from "./routes/products.router.js";
+import cartsRouter from "./routes/carts.router.js";
+import indexRouter from "./routes/index.router.js";
+import realTimeRouter from "./routes/realtimeproducts.router.js";
+import messagesRouter from "./routes/messages.router.js";
+import userRouter from "./routes/user.router.js";
+import sessionRouter from "./routes/session.router.js";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  expressSession({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+      mongoOptions: {},
+      ttl: 120,
+    }),
+  })
+);
+
+app.use(express.json()); //Middleware incorporado
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(morgan("dev"));
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "handlebars");
+
+//App middleware
+const middleware = (req, res, next) => {
+  const today = new Date();
+  const message = `📅${today.toLocaleDateString()} - ⌚${today.toLocaleTimeString()}`;
+  console.log(message);
+  next();
+};
+
+app.use(middleware);
+
+//Endpoint middlewares
+
+app.use("/", userRouter);
+app.use(
+  "/api",
+  uploader.single("thumbnails"),
+  indexRouter,
+  cartsRouter,
+  prodRouter,
+  messagesRouter,
+  sessionRouter,
+  userRouter,
+  realTimeRouter
+);
+
+//Errorhandler middleware
+
+const errorHandler = (error, req, res, next) => {
+  console.error(`Ha ocurrido un error : ${error.message}`);
+  console.error(`El stack es ${error.stack}`); //Muestra todo para saber donde esta el error
+  res.status(500).send("Algo se rompio, intente mas tarde");
+};
+
+app.use(errorHandler);
+
+export default app;
---
+++ src/routes/session.router.js
@@ -0,0 +1,63 @@
+import { Router } from "express";
+import UserModel from "../dao/models/user.model.js";
+import { createHash, isValidPassword } from "../utils.js";
+const router = Router();
+
+router.post("/session/register", async (req, res) => {
+  const { body } = req;
+  try {
+    if (!body) {
+      res.status(404).send("No data or one field wrong");
+      return;
+    } else {
+      const newUser = await UserModel.create({
+        ...body,
+        password: createHash(body.password),
+      });
+      res.status(200).redirect("/login");
+    }
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+router.post("/session/login", async (req, res) => {
+  const {
+    body: { email, password },
+  } = req;
+  try {
+    let user = await UserModel.findOne({ email });
+    if (!user) {
+      //HARDCODEO
+      if (email === "adminCoder@coder.com" && password === "adminCod3r123") {
+        user = {
+          first_name: "Coderhouse",
+          last_name: "Administrator",
+          email: "adminCoder@coder.com",
+          age: 9,
+          password: "adminCod3r123",
+          rol: "admin",
+        };
+      } else {
+        return res.status(401).send("User or password wrong");
+      }
+    }
+    const isValidPass = isValidPassword(password, user);
+    if (!isValidPass) {
+      return res.status(401).send("User or password wrong");
+    }
+    const { first_name, last_name, rol } = user;
+
+    req.session.user = { first_name, last_name, email, rol };
+    res.redirect("/api/realtimeproducts");
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+router.get("/session/logout", (req, res) => {
+  req.session.destroy((error) => {
+    res.redirect("/login");
+  });
+});
+export default router;
