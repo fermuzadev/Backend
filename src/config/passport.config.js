dc4b3a4f900183d1a9861bcd34a1a396b00c5579
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2023-11-25 12:47:17 -0300

    updates passport

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,4 +1,21 @@
 import passport from "passport";
-import local from "passport-local";
-import userService from "../dao/";
+import { Strategy as LocalStrategy } from "passport-local";
+import userService from "../dao/models/user.model.js";
 import { createHash, isValidPassword } from "../utils.js";
+import { register } from "module";
+
+const opts = {
+  userNameField: "email",
+  passReqToCallback: true,
+};
+
+const init = () => {
+  passport.use(
+    "register",
+    new LocalStrategy(opts, async (req, email, password, done) => {
+        
+
+
+    })
+  );
+};
