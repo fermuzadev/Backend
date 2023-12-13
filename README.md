b7bc2ab5bb04b1c8ecc470228f6ee62d17aaccfe
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2023-12-13 14:51:48 -0300

    updates github auth

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ Node.JS || Express.js || Websocket || Socket.io || Sweet Alert 2 || Routers || H
 #Chat => https://selective-wind-pepper.glitch.me/
 
 #Ecommerce & User Login => https://codingclothes.onrender.com/
+
+# Websocket
+Ecommerce Full Stack app with the following technologies & Chat app:
+Node.JS || Express.js || Websocket || Socket.io || Sweet Alert 2 || Routers || Handlebars || JWT || Bcrypt || Userlogin || Cookie-parser || Realtime || Render || Vercel 
+
+
+# Deploy
+
+#Chat => https://selective-wind-pepper.glitch.me/
+
+#Ecommerce & User Login => https://codingclothes.onrender.com/
--- dist/dist/utils.js
+++ dist/dist/utils.js
@@ -15,12 +15,12 @@ const storage = multer.diskStorage({
     const filename = `${Date.now()}-${file.originalname}`;
     console.log("filename", filename);
     callback(null, filename);
-  }
+  },
 });
 export const uploader = multer({
-  storage
+  storage,
 });
-export const getRandomId = array => {
+export const getRandomId = (array) => {
   let numId = parseInt(Math.random() * (10000 - 1) + 1);
   if (numId === array.id) {
     numId = parseInt(Math.random() * (10000 - 1) + 1);
@@ -28,7 +28,7 @@ export const getRandomId = array => {
     return numId;
   }
 };
-export const existFile = async path => {
+export const existFile = async (path) => {
   try {
     await fs.access(path);
     return true;
@@ -36,7 +36,7 @@ export const existFile = async path => {
     return false;
   }
 };
-export const getJSONFromFile = async path => {
+export const getJSONFromFile = async (path) => {
   if (!(await existFile(path))) {
     return [];
   } else {
@@ -67,5 +67,80 @@ export class Exception extends Error {
     this.statusCode = status;
   }
 }
-export const createHash = password => bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-export const isValidPassword = (password, user) => bcrypt.compareSync(password, user.password);
+export const createHash = (password) =>
+  bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+export const isValidPassword = (password, user) =>
+  bcrypt.compareSync(password, user.password);
+import { fileURLToPath } from "url";
+import path from "path";
+import bcrypt, { compare } from "bcrypt";
+import fs from "fs/promises";
+import multer from "multer";
+const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    const folderPath = path.join(__dirname, "../public/img");
+    console.log("Destination folder", folderPath);
+    callback(null, folderPath);
+  },
+  filename: (req, file, callback) => {
+    const filename = `${Date.now()}-${file.originalname}`;
+    console.log("filename", filename);
+    callback(null, filename);
+  },
+});
+export const uploader = multer({
+  storage,
+});
+export const getRandomId = (array) => {
+  let numId = parseInt(Math.random() * (10000 - 1) + 1);
+  if (numId === array.id) {
+    numId = parseInt(Math.random() * (10000 - 1) + 1);
+  } else {
+    return numId;
+  }
+};
+export const existFile = async (path) => {
+  try {
+    await fs.access(path);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+export const getJSONFromFile = async (path) => {
+  if (!(await existFile(path))) {
+    return [];
+  } else {
+    let content;
+    try {
+      content = await fs.readFile(path, "utf-8");
+    } catch (error) {
+      throw new Error(`The file ${path} could not be read`);
+    }
+    try {
+      return JSON.parse(content);
+    } catch (error) {
+      throw new Error(`The file ${path} hasn't a JSON format`);
+    }
+  }
+};
+export const saveJSONToFile = async (path, data) => {
+  const content = JSON.stringify(data, null, "\t");
+  try {
+    await fs.writeFile(path, content, "utf-8");
+  } catch (error) {
+    throw new Error(`The file ${path} couldn't be write`);
+  }
+};
+export class Exception extends Error {
+  constructor(message, status) {
+    super(message);
+    this.statusCode = status;
+  }
+}
+export const createHash = (password) =>
+  bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+export const isValidPassword = (password, user) =>
+  bcrypt.compareSync(password, user.password);
