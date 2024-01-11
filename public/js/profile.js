d9f9979187acdcba32cb7b256376ace79fbefede
Fernando Muzaber <fmuzaber@gmail.com>
2024-01-11 08:45:48 -0300

    updates

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,12 @@
-(function(){
-    fetch('/current')
+(function () {
+  fetch("/current")
     .then((res) => res.json())
     .then((data) => {
-            const htmlText = `<p>Nombre: ${data.firstname}</p>
+      const htmlText = `<p>Nombre: ${data.firstname}</p>
             <p>Apellido: ${data.lastname} </p>
-            <p>Email: ${data.email}</p>`
-            const span = document.getElementById('profile-span');
-            span.innerHTML = htmlText;
-        }
-    ).catch((error) => console.error('error', error))
-
-    
-})()
+            <p>Email: ${data.email}</p>`;
+      const span = document.getElementById("profile-span");
+      span.innerHTML = htmlText;
+    })
+    .catch((error) => console.error("error", error));
+})();
