3fab2f9237f46aed537f9d24b0914ab7fe7ecd9d
Fernando Muzaber <fmuzaber@gmail.com>
2024-01-22 09:06:58 -0300

    routerBase

--- src/routes/RouterBase.js
+++ src/routes/RouterBase.js
@@ -1,1 +1,40 @@
 import {Router} from 'express';
+
+
+export default class RouterBase {
+    constructor() {
+        this.router = Router();
+        this.init();
+    }
+
+    getRouter() {
+        return this.router;
+    }
+    init() {};
+    get(path, ...callback) {
+        this.router.get(path, this.applyCallBacks(callback))
+    }
+
+    post() {
+
+    }
+    put() {
+
+    }
+
+    delete() {
+
+    }
+    applyCallBacks(callbacks) {
+        return callback.map((callback) => {
+            return async(...params) => {
+                try {
+                    await callback.apply(this, params);
+                } catch (error) {
+                    console.error('Ha ocurrido un error 👀', error.message);
+                    params[1].status(500).json({message: error.message});
+                }
+            }
+        })
+    }
+}
