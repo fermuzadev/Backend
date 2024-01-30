024e452bb5c6c565d17538c05bd94a4c14f97a2b
fermuzadev <fmuzaber@economicas.uba.ar>
2024-01-30 09:21:53 -0300

    updates

--- src/dao/userManager.js
+++ src/dao/userManager.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 //!Ver la forma de crear un manager para manejar usuarios sin usar la db (admin)
 
-//!Ver la forma de crear un manager para manejar usuarios sin usar la db (admin)
-=======
-//Ver la forma de crear un manager para manejar usuarios sin usar la db (admin)
->>>>>>> origin/jwt-advanced
